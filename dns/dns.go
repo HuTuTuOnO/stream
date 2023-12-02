0359bcac3c614276e602ddba27d26fe2c709e052
HuTuTu <122086569+HuTuTuOnO@users.noreply.github.com>
2023-12-02 13:44:15 +0800

    Add files via upload

--- dns/dns.go
+++ dns/dns.go
@@ -1,1 +1,27 @@
+package dns
 
+import (
+	"context"
+	"net"
+)
+
+var (
+	Upstream string
+
+	dialer = net.Dialer{
+		Resolver: &net.Resolver{
+			PreferGo: true,
+			Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+				return net.Dial("udp", Upstream)
+			},
+		},
+	}
+)
+
+func Dial(network, address string) (net.Conn, error) {
+	return dialer.Dial(network, address)
+}
+
+func DialContext(ctx context.Context, network, address string) (net.Conn, error) {
+	return dialer.DialContext(ctx, network, address)
+}
